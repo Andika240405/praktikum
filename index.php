c6f6de0480b4aa3fc7bb2e0f56b8f17f76814612
andika <andikaabdiputra@gmail.com>
2026-06-10 21:22:08 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<html>
-<body>
-
-<h2> Tugas Pertemuan 1 - Pemrograman Web </h2>
-
-<?php
-echo "Nama: Andika Abdi Putra Septianto <br>";
-echo "NIM: 202410715256 <br>";
-echo "Kelas: F4A4 <br>";
-echo "Tanggal: 10 - Maret - 2026 <br>";
-?>
-
-</body>
-</html>
+<?php echo 'Hello Andika'; ?>
